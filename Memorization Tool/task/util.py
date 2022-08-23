3b7160cecbe62784f8854812cb84c3e6a2780040
NickEU <k@nick.tf>
2022-08-23 13:45:35 +0300

    fix(s2-persist-sql-alchemy): removed redundant if cond check

--- Memorization Tool/task/util.py
+++ Memorization Tool/task/util.py
@@ -11,6 +11,6 @@ def loop_input_until_valid_answer(start_message, valid_options):
 
 def loop_input_until_nonempty_answer(message):
     user_input = ""
-    while not user_input.strip():
+    while not user_input or not user_input.strip():
         user_input = input(message)
     return user_input
